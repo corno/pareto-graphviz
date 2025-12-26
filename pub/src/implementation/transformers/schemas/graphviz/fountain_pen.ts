46424c91a2461364ac2e9ecb6baa1d2383eca6d4
Corno Schraverus <corno@schraverus.com>
2025-12-26 09:10:29 +0100

    ...

--- pub/src/implementation/transformers/schemas/graphviz/fountain_pen.ts
+++ pub/src/implementation/transformers/schemas/graphviz/fountain_pen.ts
@@ -4,7 +4,7 @@ import * as _et from 'exupery-core-types'
 import * as d_in from "../../../../interface/generated/pareto/schemas/graphviz/data_types/source"
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/pareto/schemas/block/data_types/target"
 
-import { $$ as op_serialize_string } from "../../../serializers/primitives/text/quoted"
+import { $$ as s_quoted } from "../../../serializers/primitives/text/quoted"
 
 import * as sh from "pareto-fountain-pen/dist/shorthands/block"
 
@@ -16,12 +16,12 @@ export const Graph: _et.Transformer<d_in.Graph, d_out.Group> = ($) => {
                 sh.g.simple_block(``),
                 sh.g.simple_block(`// nodes`),
                 sh.g.sub($.nodes.deprecated_to_array(() => 1).map(($) => sh.g.nested_block([
-                    sh.b.snippet(`${op_serialize_string($.key)};`),
+                    sh.b.snippet(`${s_quoted($.key)};`),
                 ]))),
                 sh.g.simple_block(``),
                 sh.g.simple_block(`// edges`),
                 sh.g.sub($.edges.map(($) => sh.g.nested_block([
-                    sh.b.snippet(`${op_serialize_string($.from)} -> ${op_serialize_string($.to)} ;`),
+                    sh.b.snippet(`${s_quoted($.from)} -> ${s_quoted($.to)} ;`),
                 ]))),
                 sh.g.simple_block(``),
             ]),
