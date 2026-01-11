0f23c03d75bf7071da2a64d995eed020312c0299
Corno Schraverus <corno@schraverus.com>
2026-01-12 00:14:23 +0100

    ...

--- pub/src/interface/generated/pareto/schemas/graphviz/migrate_boilerplate.ts
+++ pub/src/interface/generated/pareto/schemas/graphviz/migrate_boilerplate.ts
@@ -1,47 +1,47 @@
-import * as _pi from 'pareto-core-interface'
-
-import * as _i_out from "./data_types/target"
-import * as _i_in from "./data_types/source"
-
-// **** TYPES
-
-export type _T_Graph = (
-    $$_: _i_in._T_Graph,
-    $$_p: null,
-) => _i_out._T_Graph
-
-// **** FRIENDLY NAMES FOR THE GLOBAL TYPES
-
-export type Graph = _T_Graph
-
-// **** ALIASES FOR NESTED TYPE WITH PREFIXED ROOT NAMES
-
-export namespace _T_Graph {
+import * as _pi from "pareto-core-interface"
     
-    export namespace CONTEXT {
-    }
-    export type CONTEXT = _i_in._T_Graph
+    import * as _i_out from "./data_types/target"
+    import * as _i_in from "./data_types/source"
     
-    export namespace PARAMS {
-    }
+    // **** TYPES
     
-    export namespace RESULT {
-    }
-    export type RESULT = _i_out._T_Graph
-}
-
-// *** ALIASES FOR NESTED TYPES
-
-export namespace Graph {
+    export type _T_Graph = (
+        $$_: _i_in._T_Graph,
+        $$_p: null,
+    ) => _i_out._T_Graph
     
-    export namespace CONTEXT {
-    }
-    export type CONTEXT = _i_in._T_Graph
+    // **** FRIENDLY NAMES FOR THE GLOBAL TYPES
     
-    export namespace PARAMS {
+    export type Graph = _T_Graph
+    
+    // **** ALIASES FOR NESTED TYPE WITH PREFIXED ROOT NAMES
+    
+    export namespace _T_Graph {
+        
+        export namespace CONTEXT {
+        }
+        export type CONTEXT = _i_in._T_Graph
+        
+        export namespace PARAMS {
+        }
+        
+        export namespace RESULT {
+        }
+        export type RESULT = _i_out._T_Graph
     }
     
-    export namespace RESULT {
+    // *** ALIASES FOR NESTED TYPES
+    
+    export namespace Graph {
+        
+        export namespace CONTEXT {
+        }
+        export type CONTEXT = _i_in._T_Graph
+        
+        export namespace PARAMS {
+        }
+        
+        export namespace RESULT {
+        }
+        export type RESULT = _i_out._T_Graph
     }
-    export type RESULT = _i_out._T_Graph
-}
