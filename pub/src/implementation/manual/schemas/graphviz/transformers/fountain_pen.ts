68e7a0e33e793507b8a4fccea82ef8cd516071b5
Corno Schraverus <corno@schraverus.com>
2026-01-16 09:20:56 +0100

    ...

--- pub/src/implementation/manual/schemas/graphviz/transformers/fountain_pen.ts
+++ pub/src/implementation/manual/schemas/graphviz/transformers/fountain_pen.ts
@@ -2,7 +2,7 @@ import * as _p from 'pareto-core-transformer'
 import * as _pi from 'pareto-core-interface'
 
 import * as d_in from "../../../../../interface/generated/pareto/schemas/graphviz/data"
-import * as d_out from "pareto-fountain-pen/dist/interface/generated/pareto/schemas/block/data_types/target"
+import * as d_out from "pareto-fountain-pen/dist/interface/generated/pareto/schemas/block/data"
 
 type signature = _pi.Transformer<d_in.Graph, d_out.Group>
 
