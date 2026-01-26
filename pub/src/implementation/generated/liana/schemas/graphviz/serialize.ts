22f896582ca911af61582b583911db22a3a5becb
Corno Schraverus <corno@schraverus.com>
2026-01-26 11:27:27 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/graphviz/serialize.ts
@@ -0,0 +1,10 @@
+
+import * as _p from "pareto-core/dist/serializer"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/graphviz/serialize"
+
+import * as v_serialize from "astn-core/dist/implementation/manual/schemas/sealed_target/serializers"
+
+import * as v_marshall from "./marshall"
+export const Attributes: t_signatures.Attributes = ($,) => v_serialize.Document(v_marshall.Attributes($))
+export const Graph: t_signatures.Graph = ($,) => v_serialize.Document(v_marshall.Graph($))
