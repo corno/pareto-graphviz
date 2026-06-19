5efadbaeaad0a40c6080304eda44d8b9e2e0983d
Corno Schraverus <corno@schraverus.com>
2026-06-19 22:15:51 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/high_level/low_level.ts
+++ typescript/lib/src/implementation/manual/transformers/high_level/low_level.ts
@@ -1,7 +1,7 @@
 import * as p_ from 'pareto-core/dist/implementation/transformer'
 import * as p_i from 'pareto-core/dist/interface/transformer'
 import * as p_di from 'pareto-core/dist/interface/data'
-import p_text_from_list from 'pareto-core/dist/implementation/specials/text_from_list'
+import p_text_from_list from 'pareto-core/dist/implementation/transformer/specials/text_from_list'
 
 //data types
 import * as d_in from "../../../../interface/generated/liana/schemas/high_level/data"
