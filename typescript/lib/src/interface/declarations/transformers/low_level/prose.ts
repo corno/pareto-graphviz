3421261bb496a7ea583576ce23db3b5a339499e1
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:37:47 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/low_level/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/low_level/prose.ts
@@ -1,7 +1,7 @@
 import * as p_ from 'pareto-core/implementation/transformer'
 import type * as p_i from 'pareto-core/interface/transformer'
 
-import type * as d_in from "../../../../interface/generated/liana/schemas/low_level/data.js"
+import type * as d_in from "../../../generated/liana/schemas/low_level/data.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
 
 export namespace d_parameters {
