becc2d97cb58380e500b609a38c7e9eb3dd44dd7
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:46:16 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/low_level/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/low_level/prose.ts
@@ -1,4 +1,4 @@
-import * as p_ from 'pareto-core/implementation/transformer'
+
 import type * as p_i from 'pareto-core/interface/transformer'
 
 import type * as d_in from "../../../generated/liana/schemas/low_level/data.js"
@@ -11,31 +11,31 @@ export namespace d_parameters {
 
 }
 
-export namespace interface_ {
-    export type Graph = p_i.Transformer<
-        d_in.Graph,
-        d_out.Paragraph
-    >
-    export type Statements = p_i.Transformer_With_Parameter<
-        d_in.Statements,
-        d_out.Phrase,
-        d_parameters.Parameters
-    >
-    export type ID = p_i.Transformer<
-        d_in.ID,
-        d_out.Phrase
-    >
-    export type Attributes = p_i.Transformer<
-        d_in.Attributes,
-        d_out.Phrase
-    >
-    export type Node_ID = p_i.Transformer<
-        d_in.Node_ID,
-        d_out.Phrase
-    >
-    export type Subgraph = p_i.Transformer_With_Parameter<
-        d_in.Subgraph,
-        d_out.Phrase,
-        d_parameters.Parameters
-    >
-}
+
+export type Graph = p_i.Transformer<
+    d_in.Graph,
+    d_out.Paragraph
+>
+export type Statements = p_i.Transformer_With_Parameter<
+    d_in.Statements,
+    d_out.Phrase,
+    d_parameters.Parameters
+>
+export type ID = p_i.Transformer<
+    d_in.ID,
+    d_out.Phrase
+>
+export type Attributes = p_i.Transformer<
+    d_in.Attributes,
+    d_out.Phrase
+>
+export type Node_ID = p_i.Transformer<
+    d_in.Node_ID,
+    d_out.Phrase
+>
+export type Subgraph = p_i.Transformer_With_Parameter<
+    d_in.Subgraph,
+    d_out.Phrase,
+    d_parameters.Parameters
+>
+
