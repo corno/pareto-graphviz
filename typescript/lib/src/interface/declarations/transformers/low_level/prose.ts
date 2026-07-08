2affe4a1df390db02a45649fecc9a787265458c7
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:47:36 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/low_level/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/low_level/prose.ts
@@ -1,5 +1,5 @@
 
-import type * as p_i from 'pareto-core/interface/transformer'
+import type * as p_ from 'pareto-core/interface/transformer'
 
 import type * as d_in from "../../../generated/liana/schemas/low_level/data.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
@@ -12,28 +12,28 @@ export namespace d_parameters {
 }
 
 
-export type Graph = p_i.Transformer<
+export type Graph = p_.Transformer<
     d_in.Graph,
     d_out.Paragraph
 >
-export type Statements = p_i.Transformer_With_Parameter<
+export type Statements = p_.Transformer_With_Parameter<
     d_in.Statements,
     d_out.Phrase,
     d_parameters.Parameters
 >
-export type ID = p_i.Transformer<
+export type ID = p_.Transformer<
     d_in.ID,
     d_out.Phrase
 >
-export type Attributes = p_i.Transformer<
+export type Attributes = p_.Transformer<
     d_in.Attributes,
     d_out.Phrase
 >
-export type Node_ID = p_i.Transformer<
+export type Node_ID = p_.Transformer<
     d_in.Node_ID,
     d_out.Phrase
 >
-export type Subgraph = p_i.Transformer_With_Parameter<
+export type Subgraph = p_.Transformer_With_Parameter<
     d_in.Subgraph,
     d_out.Phrase,
     d_parameters.Parameters
