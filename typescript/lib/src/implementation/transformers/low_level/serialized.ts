80c4e8f3315c6666a7bb8263d488e11e60064970
Corno Schraverus <corno@schraverus.com>
2026-07-18 17:12:46 +0200

    pdt: published version 0.2.59

---
+++ typescript/lib/src/implementation/transformers/low_level/serialized.ts
@@ -0,0 +1,25 @@
+import * as p_ from 'pareto-core/implementation/transformer'
+
+//schemas
+import type * as s_in from "../../../interface/schemas/low_level.js"
+import type * as s_out from "../../../interface/schemas/serialized.js"
+import type * as s_parameters from "pareto-fountain-pen/interface/schemas/paragraph_serialization"
+
+namespace declarations {
+    export type Graph = p_.Transformer_With_Parameter<
+        s_in.Graph,
+        s_out.Lines,
+        s_parameters.Parameters
+    >
+}
+
+
+//dependencies
+import * as t_to_paragraph from "./paragraph.js"
+import * as t_paragraph_to_serialized from "pareto-fountain-pen/_implementation/transformers/paragraph/serialized"
+
+
+export const Graph: declarations.Graph = ($, $p) => t_paragraph_to_serialized.Paragraph(
+    t_to_paragraph.Graph($),
+    $p
+)
