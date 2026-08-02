1ca282f0b20b8840edf477ba845f05b19bfa4a40
Corno Schraverus <corno@schraverus.com>
2026-08-02 14:36:32 +0200

    pdt: ...

--- typescript/lib/src/schemas/high_level/transformers/paragraph.ts
+++ typescript/lib/src/schemas/high_level/transformers/paragraph.ts
@@ -4,7 +4,7 @@ import type * as p_i from 'pareto-core/implementation/transformer'
 import type * as s_in from "../schema.js"
 
 //dependencies
-import * as t_low_level_to_prose from "../../low_level/transformers/paragraph.js"
+import * as t_low_level_to_paragraph from "../../low_level/transformers/paragraph.js"
 import * as t_to_low_level from "./low_level.js"
 import type * as s_out from "pareto-fountain-pen/modules/paragraph/schemas/paragraph/schema"
 
@@ -12,4 +12,4 @@ import type * as s_out from "pareto-fountain-pen/modules/paragraph/schemas/parag
 export const Graph: p_i.Transformer<
     s_in.Graph,
     s_out.Paragraph
-> = ($) => t_low_level_to_prose.Graph(t_to_low_level.Graph($))
+> = ($) => t_low_level_to_paragraph.Graph(t_to_low_level.Graph($))
